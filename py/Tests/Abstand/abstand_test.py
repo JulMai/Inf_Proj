5198669b80f421ab97f19b91eea7d35c21917b51
JulMai <mai.julian270@gmail.com>
2021-11-28 14:12:36 +0100

    Tests für Datenstruktur Autos auf der Strecke

--- py/Tests/Abstand/abstand_test.py
+++ py/Tests/Abstand/abstand_test.py
@@ -4,13 +4,17 @@ import time
 import logging
 from track.track import Track
 from track.trackPieceFactory import get_TrackPiece
+from read_lap_test_callback import scan_track_with_lanes
 
+car_on_track = False
 
 class Car_Logger(Thread):
     location = 0
     piece = 0
     speed = 0
     clockwise = False
+
+    
     
     def run(self):
         self.track_ids = []
@@ -19,103 +23,85 @@ class Car_Logger(Thread):
           
 
     def locationChangeCallback(self, addr, location, piece, speed, clockwise):
-        if self.check_id(piece) != '' and not (piece == 34 and len(self.track_ids)==0):
-            if piece == self.piece:
-                if (location > self.location and clockwise == True) or (location < self.location and clockwise == False):
-                    self.track_ids.append((piece, location))               
-            else:
-                self.track_ids.append((piece, location))
+        global track
+        global car_on_track
+        
+        if car_on_track == True:
+            remove_car_from_track(car1)
+        
+        for x in track:
+            if piece == x[0]:
+                for l in range(len(x[1])):
+                    if x[1][l][0] == location:
+                        x[1][l][1] = car1
+                        car_on_track = True
+                        #print("Car1 assigned to Piece {0}, Location: {1}".format(piece, location))
+                        print(str(track))
+                      
+
+
+        
         
         self.piece = piece
         self.location = location
         self.speed = speed
         self.clockwise = clockwise
 
-    def check_id(self, id):
-        return {        
-            10: True,
-            17: True,
-            18: True,
-            20: True,
-            23: True,
-            33: True,
-            34: True,
-            36: True,
-            39: True,
-            48: True
-        }.get(id, '')     
-
-
-def scan_track(car, speed=500):
-
-    car_logger = Car_Logger(kwargs={'car': car})
-    car_logger.start()
-
-    car.changeSpeed(speed, 1000)
-
-    time.sleep(1)
-
-    car.changeSpeed(0, 1000)
-    
-
-    has_33 = False
-    has_34 = False
-
-    track_ids = car_logger.track_ids
-
-    for id in track_ids:
-        if id == 33:
-            has_33 = True
-        if id == 34:
-            has_34 = True
-
-    if has_33 == False:
-        track_ids.reverse()
-        track_ids.append(33)
-        track_ids.reverse()
-    
-    if has_34 == False:
-        track_ids.append(34)
-
-    car_logger.join()
-    del car_logger
-
-    return track_ids
-
-    
-
-#car = Vehicle("D9:A6:FA:EB:FC:01")
-car1 = Vehicle("EC:33:B4:DB:9E:C8")
-car_logger = Car_Logger(kwargs={'car': car1})
-car_logger.start()
-
-car1.changeSpeed(400, 1000)
-
-time.sleep(12)
-
-car1.changeSpeed(0, 1000)
-
-track_t = car_logger.track_ids
-
-car_logger.join()
-
-track_ids = []
 
 track = []
 
-for t in track_t:
-    id = t[0]
-    location = t[1]
-    piece = get_TrackPiece(id)
-    lane = []
-    for l in piece.coordinates:
-        for c in l:
-            if c == location:
-                lane = l
-                break
-    track.append([id, lane])
-    print("")
+def remove_car_from_track(car):
+    global track
+    global car_on_track
     
+    
+    for x in track:
+        for y in x[1]:
+            if y[1] == car:
+                    y[1] = None
+                    print("")
 
 
-print("")
+def get_track_with_lanes(track_before):
+    track_after = []
+
+    for t in track_before:
+        id = t[0]
+        location = t[1]
+        piece = get_TrackPiece(id)
+        lane = []
+        for locations in piece.coordinates:
+            for c in locations:
+                if c == location:
+                    for i in range(len(locations)):
+                        locations[i] = [locations[i], None]
+                    lane = locations
+                    break
+        track_after.append([id, lane])
+
+    return track_after
+
+
+if __name__ == "__main__":
+    #car = Vehicle("D9:A6:FA:EB:FC:01")
+    car1 = Vehicle("EC:33:B4:DB:9E:C8")
+
+    track_t = scan_track_with_lanes(car1)
+
+    
+    track = get_track_with_lanes(track_t)
+
+    time.sleep(1)
+
+    car_logger = Car_Logger(kwargs={'car': car1})
+    car_logger.start()
+
+    car1.changeSpeed(400, 1000)
+
+    time.sleep(10)
+
+    car1.changeSpeed(0, 1000)
+
+    car_logger.join()
+
+    print("")
---
+++ py/drive_to_start.py
@@ -0,0 +1,44 @@
+from Tests.vehicle.vehicle import Vehicle
+from threading import Thread
+import os
+import time
+
+
+class Car_Logger(Thread):
+    location = 0
+    piece = 0
+    speed = 0
+    clockwise = False
+
+    
+    
+    def run(self):
+        self.track_ids = []
+        car = self._kwargs['car']
+        car.setLocationChangeCallback(self.locationChangeCallback)  
+          
+
+    def locationChangeCallback(self, addr, location, piece, speed, clockwise):
+        self.piece = piece
+        self.location = location
+        self.speed = speed
+        self.clockwise = clockwise
+
+
+car = Vehicle("EC:33:B4:DB:9E:C8")
+#car = Vehicle("D9:A6:FA:EB:FC:01")
+
+car_l = Car_Logger(kwargs={'car': car})
+car_l.start()
+
+car.changeSpeed(500, 1000)
+
+while(car_l.piece != 34):
+    pass
+
+car.changeSpeed(0, 1000)
+time.sleep(1)
+car_l.join()
+del car_l
+del car
+os._exit(0)
