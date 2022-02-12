c1d055d3bbf74377ea460869dcac2ee76f247e87
JulMai <64149204+JulMai@users.noreply.github.com>
2022-02-12 11:47:50 +0100

    Update Car_Logger_Scan.py

--- Car_Logger/Car_Logger_Scan.py
+++ Car_Logger/Car_Logger_Scan.py
@@ -1,3 +1,5 @@
+# Class for receiving Live-Car-Updates, Scanning the Track and writing the TrackPieces to a dictionary
+
 from threading import Thread
 import logging
 import time
@@ -15,7 +17,8 @@ class Car_Logger_Scan(Thread):
         self.car = self._kwargs['car']
         self.car.setLocationChangeCallback(self.locationChangeCallback)
         self.track_ids = []
-
+    
+    # gets called everytime there is an update sent by the car
     def locationChangeCallback(self, addr, location, piece, speed, clockwise):
         self.car.location = location
         self.car.piece = piece
@@ -31,30 +34,28 @@ class Car_Logger_Scan(Thread):
         self.speed = speed
         self.clockwise = clockwise   
 
+# takes care of starting the Thread for Scanning
 def setup_and_start_Car_Logger(car):
     c_l = Car_Logger_Scan(kwargs={'car': car})
     c_l.start()
     logging.info("Started Car_Logger_Scan-Thread for Car: {0}".format(car.addr))
     return c_l
 
+# stops the Scanning-Thread
 def stop_and_cleanup_Car_Logger(car_Logger):
     car_Logger.join()
     logging.info("Stopped Car_Logger for Car: {0}".format(car_Logger.car.addr))
     del car_Logger
     
-
+# Converts a list of TrackPiece-Numbers to a Dictionary
+# Key: IIPPLL (I: Index, P: Position/TrackPiece, L: Location inside TrackPiece)
+# Value: None, Space for Car-Obj or MAC-Address
 def track_to_dict(track):
     track_dict = {}
     prev_piece = 0
     prev_loc = 0
     i = 0
     for l in track:
-        #if prev_piece != 0:
-        #    if l[0] == prev_piece:
-        #        if abs(l[1] - prev_loc) != 1:
-        #            i += 1
-        #    else:
-        #        i += 1
         str = f"{i:02d}{l[0]:02d}{l[1]:02d}"
         track_dict[str] = None
         prev_piece = l[0]
@@ -62,22 +63,27 @@ def track_to_dict(track):
         i += 1
     return track_dict
 
+# Function to scan the TrackPieces into a list
 def scan_track(car, speed=400):
-    # Position Car properly to start scan
+    # position Car properly to start scan (most left lane and piece 33/34)
     drive_to_most_left_lane(car)
     drive_to_start(car)
 
     car_logger = setup_and_start_Car_Logger(car)
-
+    
+    # Accelerate the car
     car.changeSpeed(speed, 1000)
-
+    
+    # keep driving until the car is back at the start/finish
     while (car_logger.piece != 34):
         pass
     
+    # stop the car
     car.changeSpeed(0, 1000)
     time.sleep(1)
     track = car_logger.track_ids
-
+    
+    # if the TrackPieces 33 or 34 have not been scanned correctly, they get corrected here
     has_33 = False
     has_34_0 = False
     has_34_1 = False
